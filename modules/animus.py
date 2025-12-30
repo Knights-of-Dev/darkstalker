0e3d413cfaffb030265b0a690dc9e219625c2a13
🏳‍🌈 O9Creeps 🐯 <zslaba8949@inst.hcpss.org>
2025-12-30 03:06:00 +0000

    43

--- modules/animus.py
+++ modules/animus.py
@@ -1,8 +1,23 @@
 # i have hippopotomonstrosesquippedaliophobia
 
+import math
+# this may be useful later.
+
 class object:
-    def __init__(self, livestat, a, b, c):
+    def __init__(self, livestat=None, a=None, b=None, c=None):
         self.livestat = livestat
         self.a = a
-        self.a = b
-        self.a = c
+        self.b = b
+        self.c = c
+        self.isEnchanted = False
+    def getData(self):
+        return (self.livestat, self.a, self.b, self.c)
+    def enchant(self):
+        self.isEnchanted = True
+    def disenchant(self):
+        self.isEnchanted = False
+    def command(self, cmd):
+        if self.isEnchanted:
+            return "The object shimmers with a magical aura."
+        else:
+            return "Error: Object is not enchanted."
