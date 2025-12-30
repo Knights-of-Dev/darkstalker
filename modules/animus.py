27823bbf348b130c8a65244172d380094536a75d
🏳‍🌈 O9Creeps 🐯 <zslaba8949@inst.hcpss.org>
2025-12-30 02:35:55 +0000

    rtgd

--- modules/animus.py
+++ modules/animus.py
@@ -1,4 +1,8 @@
 # i have hippopotomonstrosesquippedaliophobia
 
 class object:
-    def __init__(self, ):
+    def __init__(self, livestat, a, b, c):
+        self.livestat = livestat
+        self.a = a
+        self.a = b
+        self.a = c
