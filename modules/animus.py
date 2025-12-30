f22cb2dd97ff14ea34d7e257594f9354150620a9
🏳‍🌈 O9Creeps 🐯 <zslaba8949@inst.hcpss.org>
2025-12-30 19:59:32 +0000

    1qaz

--- modules/animus.py
+++ modules/animus.py
@@ -10,6 +10,12 @@ import math
 global debug; debug = False
 # i didnt know you could put a semicolon IN PYTHON
 
+def ifelse(con, tru=True, fal=False):
+    if con:
+        return tru
+    else:
+        return fal
+
 class object:
     def __init__(self, livestat=None, a=None, b=None, c=None):
         self.livestat = livestat
@@ -21,23 +27,30 @@ class object:
         if self.a != None: self.objectName = f"{self.objectName}.{self.a}"
         if self.b != None: self.objectName = f"{self.objectName}.{self.b}"
         if self.c != None: self.objectName = f"{self.objectName}.{self.c}"
-    def getData(self):
+    # i feel like this is a good place to stop and reflect on my life choices
+    def getData(self, autoprint=False):
+        if autoprint or debug: print((self.livestat, self.a, self.b, self.c))
         return (self.livestat, self.a, self.b, self.c)
-    def enchant(self):
+    def enchant(self, autoprint=False):
         self.isEnchanted = True
-        if debug: print(f"Object {self.objectName} has been enchanted.")
-    def disenchant(self):
+        if debug or autoprint: print(f"Object {self.objectName} has been enchanted.")
+    def disenchant(self, autoprint=False):
         self.isEnchanted = False
-        if debug: print(f"Object {self.objectName} has been disenchanted.")
-    def command(self, cmd):
+        # debug and autoprint are pretty good together :3
+        if debug or autoprint: print(f"Object {self.objectName} has been disenchanted.")
+    def command(self, cmd, autoprint=False):
         if self.isEnchanted:
-            return f"Object {self.objectName} executed command: {cmd}"
+            result = f"Object {self.objectName} executed command: {cmd}"
+            if autoprint or debug: print(result)
+            return result
         else:
-            return "Error: Object is not enchanted."
+            error_msg = "Error: Object is not enchanted."
+            if autoprint or debug: print(error_msg)
+            return error_msg
         
-
-debug = bool(input("Debug?"))
-# ask it because yes
-a = object("dragon", "icewing")
+a = input("Enable debug mode? (true/false): ")
+debug = ifelse(a.lower() == "true")
+print(debug)
+a = object("dragon", "icewing", "all")
 a.enchant()
-print(a.command("fly to the north"))
+a.command("fly to the north")
